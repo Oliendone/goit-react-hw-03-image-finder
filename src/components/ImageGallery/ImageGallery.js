7376eea3fff21ac85fc43125cddbaaf24e945db7
Nikita <Zungarcheg@gmail.com>
2020-09-20 20:25:11 +0300

    fixes

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
 export default function ImageGallery({ images, openModal }) {
   return (
